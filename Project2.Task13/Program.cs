7f40ece05a77c0a2e76f621c1d0df42db0dfec98
EkaterinaVitalievna <zinovieva.k@mail.ru>
2022-07-13 01:47:54 +0300

    Выводит третью цифру или сообщает, что её нет

---
+++ Project2.Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿// Выводит третью цифру числа или сообщает, что её нет
+Console.WriteLine("Введите число");
+
+string NUMBERS = Console.ReadLine();
+
+if (NUMBERS.Length < 3)
+{
+    Console.WriteLine("Третьей цифры в числе нет");
+}
+else
+{
+    Console.WriteLine("Третья цифра числа " + NUMBERS[2]);
+}
