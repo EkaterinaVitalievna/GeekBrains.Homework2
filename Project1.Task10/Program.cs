287e9d1391a5b898970de85239b34e2d69a00b28
EkaterinaVitalievna <zinovieva.k@mail.ru>
2022-07-13 01:03:45 +0300

    Программа принимает трёхзначное число и показывает вторую цифру

---
+++ Project1.Task10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа принимает трехзначное число и показывает вторую цифру
+Console.WriteLine("Введите трёхзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string SecondSymbol(int numb)
+{
+    if (numb >= 100 && numb < 1000)
+    {
+        int step1 = numb / 10;
+        int step2 = step1 % 10;
+        return ("Второе число " + step2);
+    }
+    else
+    return ("Введённое число не является трёхзначным");
+}
+Console.WriteLine(SecondSymbol(num));
