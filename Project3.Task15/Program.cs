c92df967d172d08066969b814ef8e6078ca8b152
EkaterinaVitalievna <zinovieva.k@mail.ru>
2022-07-13 02:16:26 +0300

    Цифра дня недели - выходной или будний?

---
+++ Project3.Task15/Program.cs
@@ -0,0 +1,20 @@
+﻿// Принимает на вход цифру, обозначающую день недели и проверяет, является ли цифра выходным днём
+
+Console.WriteLine("Какой по счёту день недели?");
+int day = Convert.ToInt32(Console.ReadLine());
+
+string Week (int dayNumber)
+{
+    if (dayNumber >= 1 && dayNumber <= 5)
+    {
+        return "Введённый день - будний";
+    }
+    else if (dayNumber <= 6 && dayNumber >= 7)
+    {
+        return "Введенный день - выходным";
+    }
+    else
+        return "Error";
+}
+
+Console.WriteLine(Week(day));
